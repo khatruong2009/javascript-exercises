7dd35d3a94d6d07debca0efc2f89d558a16ffbe8
Kha Truong <khatruong2009@gmail.com>
2020-06-13 12:26:26 -0400

    saving progress from removeFromArrays project

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, rem) {
+ let n = arr.indexOf(rem);
+ let newStr = arr.splice(n, 1);
+ return arr;
 }
 
 module.exports = removeFromArray
