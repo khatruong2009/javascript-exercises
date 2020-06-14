d626221521c0f7002e676b41cddf7c046844a192
Kha Truong <khatruong2009@gmail.com>
2020-06-14 17:21:12 -0400

    finished the tempConversion assignment

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c;
+  c = (f - 32) * (5/9);
+  if (c == 0) {
+    return c;
+  } else {
+    c = c.toFixed(1);
+    return parseFloat(c);
+  }
 }
 
-const ctof = function() {
-
-}
+const ctof = function(c) {
+  let f;
+  f = (c * (9/5)) + 32;
+  if (f == 0) {
+    return c;
+  } else {
+    f = f.toFixed(1);
+    return parseFloat(f);
+}}
 
 module.exports = {
   ftoc,
