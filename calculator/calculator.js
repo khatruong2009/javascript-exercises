5d8dd14d9fe3a1d3355e410596f88db4edae9ecd
Kha Truong <khatruong2009@gmail.com>
2020-06-20 11:58:02 -0400

    finished the calculator assignment

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (arr) {
+	if (arr.length === 1) {
+		return arr[0];
+	} else {
+		return arr.reduce(function(a,b) {
+			return a + b;
+		}, 0);
+	}
+	}
+
+
+function multiply (arr) {
+	let product = 1;
+	for (i = 0; i < arr.length; i++) {
+		product = product * arr[i];
+	}
+	return product;
+	}
+
+
+function power(num1, num2) {
+	return num1 ** num2;
 }
 
-function multiply () {
-	
-}
-
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(num) {
+	let total = 1;
+	for (i = num; i > 0; i--) {
+		total = total * i;
+	}
+	return total;
 }
 
 module.exports = {
