9859a06aed75a24c389c726a8a70b86fe257bc0a
Kha Truong <khatruong2009@gmail.com>
2020-06-21 14:11:14 -0400

    finished the palindromes assignment

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+ let regex = /[^A-Za-z]/g;
+ let modStr = str.replace(regex, "").toLowerCase();
+ if (modStr.split("").reverse().join("") == modStr) {
+  return true;
+ } else return false;
 }
 
 module.exports = palindromes
