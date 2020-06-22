676976d6c158fe3a248090375b54d6a45173ff7f
Kha Truong <khatruong2009@gmail.com>
2020-06-22 11:45:33 -0400

    finished fibonacci sequence assignment

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+ let fibo = [0];
+ let prevNum = 0;
+ let currentNum = 1;
+ let temp;
 
-}
+ if (num >= 0) {
+  for (i = 0; i < 500; i++) {
+   temp = currentNum;
+   currentNum = prevNum + currentNum;
+   prevNum = temp;
+   fibo.push(prevNum);
+  }
+  return fibo[num];
+ } else return "OOPS";
+ }
+
+
 
 module.exports = fibonacci
