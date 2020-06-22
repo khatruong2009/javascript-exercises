7147e86b4d4a7ade0e50046de37a9e0e69b24e00
Kha Truong <khatruong2009@gmail.com>
2020-06-22 12:27:42 -0400

    finished get the titles assignment

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+ let titles = [];
+ for (i = 0; i < books.length; i++) {
+  let title = books[i].title;
+  titles.push(title);
+ }
 
+ return titles;
 }
 
 module.exports = getTheTitles;
